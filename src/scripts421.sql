eeb8fe97ab5f471ce273b6d4067a7a0503914164
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-11-06 20:55:03 +0400

    ДЗ 4.2 v3

--- src/scripts421.sql
+++ src/scripts421.sql
@@ -2,7 +2,7 @@ ALTER TABLE student
 ADD CONSTRAINT age_constraint CHECK (age >=16),
 ADD CONSTRAINT name_unique UNIQUE (name);
 
-ALTER TABLE student ALTER COLUMN name NOT NULL,
+ALTER TABLE student ALTER COLUMN name SET NOT NULL,
 
 ALTER TABLE faculty
 ADD CONSTRAINT name_color_unique UNIQUE (name, color);
--- src/scripts422.sql
+++ src/scripts422.sql
@@ -1,5 +1,5 @@
-CREATE TABLE person (id SERIAL, name TEXT, age INTEGER CHECK (age > 0), driver BOOLEAN NOT NULL);
+CREATE TABLE person (id INTEGER PRIMARY KEY, name TEXT, age INTEGER CHECK (age > 0), driver BOOLEAN NOT NULL);
 
-CREATE TABLE person_car (person_id SERIAL, car_id SERIAL);
+CREATE TABLE person_car (person_id INTEGER REFERENCES person (id), car_id INTEGER REFERENCES car (id));
 
-CREATE TABLE car (id SERIAL, brand TEXT NOT NULL, model TEXT NOT NULL, price NUMERIC (16,2) CHECK (price > 0));
+CREATE TABLE car (id INTEGER PRIMARY KEY, brand TEXT NOT NULL, model TEXT NOT NULL, price NUMERIC (16,2) CHECK (price > 0));
