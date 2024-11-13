a316c87e82508605dae43c2cddccea48953475c5
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-11-13 12:49:04 +0400

    ДЗ 4.4

--- src/main/java/ru/hogwarts/school/services/PortServiceTest.java
+++ src/main/java/ru/hogwarts/school/services/PortServiceTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("test")
+@Profile("!production")
 public class PortServiceTest implements PortService{
 
     @Value("${server.port}")
